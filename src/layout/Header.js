817099e78fae7b4c9859e16da66308de2708285e
LalaIAm <lthorud12@gmail.com>
2024-05-08 22:53:02 -0500

    hero

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -2,47 +2,77 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/icons/logo.svg';
 
-
 const Header = () => {
   return (
-    <header className='header'>
-      <Link to='/' className='header-logo'>
-        <img className='logo' src={logo} alt='logo' />
-      </Link>
-      <div className='right-header-nav'>
-        <div className='header-menu'>
-          <Link to='/about' className='header-link'>
-            About
+    <header className='navbar navbar-expand-md'>
+      <div className='container'>
+        <div className='col-lg-6'>
+          <Link className='navbar-brand' to='#'>
+            <img src={logo} alt='logo' />
           </Link>
-          <Link to='/blog' className='header-link'>
-            Blog
-          </Link>
-          <Link to='/contact' className='header-link'>
-            Contact
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarToggler'
+            aria-controls='navbarToggler'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon'></span>
+          </button>
+        </div>
+        <div className='collapse navbar-collapse col-lg-6 justify-content-end' id='navbarToggler'>
+          <ul className='navbar-nav mb-2 mb-lg-0 site-links'>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/about'>
+                About
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/blog'>
+                Blog
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/contact'>
+                Contact
+              </Link>
+            </li>
+          </ul>
+          <div className='divider' />
+          <ul className='navbar-nav mb-2 mb-lg-0 social-links'>
+            <li className='nav-item'>
+              <Link className='nav-link' to='#'>
+                <i className='icon-facebook'></i>
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='#'>
+                <i className='icon-instagram'></i>
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/contact'>
+                <i className='icon-twitter'></i>
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/contact'>
+                <i className='icon-mail'></i>
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/contact'>
+                <i className='icon-rss'></i>
+              </Link>
+            </li>
+          </ul>
+          <div className='divider'></div>
+          <Link to='#' className='nav-link'>
+            <i className='icon-search'></i>
           </Link>
         </div>
-        <div className='divider' />
-        <div className='social-menu'>
-          <Link to='#' className='social-link'>
-            <i className='icon-rss'></i>
-          </Link>
-          <Link to='#' className='social-link'>
-            <i className='icon-facebook'></i>
-          </Link>
-          <Link to='#' className='social-link'>
-            <i className='icon-instagram'></i>
-          </Link>
-          <Link to='#' className='social-link'>
-            <i className='icon-twitter'></i>
-          </Link>
-          <Link to='#' className='social-link'>
-            <i className='icon-mail'></i>
-          </Link>
-        </div>
-        <div className='divider' />
-        <Link to='#' className='social-link'>
-          <i className='icon-search'></i>
-        </Link>
       </div>
     </header>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>
-      Home Page
+    <div className='page home-page m-0 p-0'>
+      <section id='hero'>
+        <div className='container hero-container'>
+          <div className='row pt-5'>
+            <div className='pt-5 col-xl-7 col-md-9 col-sm-12'>
+              <h1>
+                Elevate Life
+                <br />
+                with Style.
+              </h1>
+              <p>
+                Explore a curated blend of style and purpose at THE LIFESTYLED,{' '}
+                <br />
+                where every post is a step towards a more inspired and elevated
+                life.
+              </p>
+              <button className='btn btn-primary'>
+                Read Blog
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
